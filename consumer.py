df801a4966519a1af697d3d6df41c0098b79b4f9
mike <mchlwlsn22@gmail.com>
2024-10-26 13:08:44 -0600

    created plan

---
+++ consumer.py
@@ -0,0 +1,20 @@
+import boto3
+
+# take in command line arguments
+
+# begin loop to request objects
+
+# request objects
+
+# process JSON request
+
+# call different function depending on request
+
+# implement create function
+
+# two different functions, one to make all data into a string and put it in S3 bucket
+# one to put data in DynmoDB 
+
+# add logging
+
+# create unit tests
