80de195fa43b356d560c685f7a389c7154fda55f
mike <mchlwlsn22@gmail.com>
2024-10-26 13:46:30 -0600

    defined initial class layout

--- consumer.py
+++ consumer.py
@@ -18,16 +18,49 @@ import time
 
 # create unit tests
 
-class Consumer:
-    def __init__():
+class S3_Requester():
+    def __init__(self, bucket_name):
+        self.s3 = boto3.resouce('s3')
+        self.request_bucket = self.s3.Bucket(bucket_name)
+
+    def retrieve(self):
         pass
 
+class Consumer:
+    def __init__(self, request, store):
+        self.request = request
+        self.store = store
+
     def run(self):
-        while True:
-            if self.request():
-                pass
-            else:
-                time.sleep(0.1)
+        try:
+            while True:
+                if self.request():
+                    pass
+                else:
+                    time.sleep(0.1)
+        except KeyboardInterrupt:
+            exit(0)
+
+    def request_data(self):
+        return self.request.retrieve()
+
+    def create(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def update(self):
+        pass
+
+class S3_Consumer(Consumer):
+    def __init__(self, request, store):
+        super(self, S3_Requester(request), store)
+        s3 = boto3.resource('s3')
+        self.bucket = s3.Bucket(store)
+
+    def request_data(self):
+        
 
 
 def initialize_parser():
@@ -41,9 +74,9 @@ if __name__ == "__main__":
     parser = initialize_parser()
     args = parser.parse_args()
     if args.request and args.store_s3:
-        consumer = Consumer(args.request, args.store_s3)
+        consumer = Consumer(args.request, args.store_s3, 0)
         consumer.run()
     elif args.request and args.store_db:
-        consumer = Consumer(args.request, args.store_db)
+        consumer = Consumer(args.request, args.store_db, 1)
         consumer.run()
 
